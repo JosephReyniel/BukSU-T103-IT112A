1734729ad19969301b627d66f6b0941e13933071
JosephReyniel <73882075+JosephReyniel@users.noreply.github.com>
2020-11-26 19:25:58 +0800

    Add files via upload

---
+++ Check if the Letter is Vowel or Consonant.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char letter;
+    printf("Enter a letter: ");
+    scanf("%c", &letter );
+
+    if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter== 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+{
+    printf("'%c' is vowel", letter);
+}
+    else
+    {
+        printf("'%c' is consonant", letter);
+    }
+    return 0;
+}
