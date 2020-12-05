08e1d52a2a08c2e2ac042b60209aa400565e55e5
JosephReyniel <73882075+JosephReyniel@users.noreply.github.com>
2020-12-05 22:00:57 +0800

    Add files via upload

---
+++ Final Task ( Looping Number 4 ) Number Pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main()
+{
+   int rows,num,space,column,count=1;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(num =1; num<=rows; num++)
+       {
+        for(space= -10; space<=rows-num; space++)
+        {
+            printf(" ");
+        }
+        for(column=1; column<=num; column++)
+        {
+            printf(" %d",count++);
+        }
+        printf("\n");
+    }
+
+     return 0;
+}
+
